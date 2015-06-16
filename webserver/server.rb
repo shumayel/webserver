79f7fa3747224844e1a794cc26712185b06b1296
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-16 15:31:18 -0400

    First commit for Web App

---
+++ webserver/server.rb
@@ -0,0 +1,31 @@
+require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
+
+host = 'localhost'
+port = 2000
+
+server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
+puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
+
+loop do                                             # Server runs forever
+  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
+
+  lines = []
+  while (line = client.gets) && !line.chomp.empty?  # Read the request and collect it until it's empty
+    lines << line.chomp
+  end
+  puts lines                                        # Output the full request to stdout
+
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
+
+  if File.exists?(filename)
+  response_body = File.read(filename)
+else
+  response_body = "File Not Found\n" # need to indicate end of the string with \n
+end
+
+filename = "response.html"
+response = File.read(filename)
+
+client.puts(response)                    # Output the current time to the clien
+client.close                                      # Disconnect from the client
+end
