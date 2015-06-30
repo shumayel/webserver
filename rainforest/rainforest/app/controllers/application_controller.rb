c89560a2bb0301de2ed0741f0fd4bffbabaa124a
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-30 16:47:49 -0400

    Adding authorization to ensure users are logged in before they create reviews

--- rainforest/rainforest/app/controllers/application_controller.rb
+++ rainforest/rainforest/app/controllers/application_controller.rb
@@ -9,4 +9,11 @@ private
   end
 
   helper_method :current_user
+
+def ensure_logged_in
+unless current_user
+    flash[:alert] = "Please log in"
+    redirect_to new_session_path
+    end
+    end
 end
