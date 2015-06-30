321b72a9f0a2cf71a95f9e6b14a38cfd3c7ddb28
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-30 13:01:45 -0400

    Generating the products controller and views

--- rainforest/rainforest/config/routes.rb
+++ rainforest/rainforest/config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/edit'
+
+  # get 'products/index'
+
+  # get 'products/show'
+
+  # get 'products/new'
+
+  # get 'products/edit'
+
+resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
